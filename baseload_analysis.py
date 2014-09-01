a2763b30fecf932c290f4ac942a23cf22ac693b2
Graeme Stuart <ggstuart@dmu.ac.uk>
2014-09-01 12:23:11 +0100

    Updates to boxplot (sorting) optionally hide plots

--- baseload_analysis.py
+++ baseload_analysis.py
@@ -7,6 +7,10 @@ from pandas import DataFrame
 logging.basicConfig(level=logging.INFO)
 
 do_subset = 0
+do_raw = False
+do_baseload_lines = False
+do_baseload_areas = False
+do_hist = False
 
 adapter = SmartspacesCSVAdapter()
 datasets = [
@@ -33,7 +37,7 @@ datasets = [
     {'lbl': 'Leicester Leys Leisure Centre', 'id': 64},
     {'lbl': 'New Leicester Central Library', 'id': 68},
     {'lbl': 'New Parks Leisure Centre', 'id': 71},
-#    {'lbl': 'New Walk Museum', 'id': 75},
+#    {'lbl': 'New Walk Museum', 'id': 75},                  #bad data
     {'lbl': 'Spence Street Leisure Centre', 'id': 78}
 ]
 
@@ -64,11 +68,14 @@ for dataset in datasets:
     mymin = max(dataset['dataframe'].index.min(), dataset['daily_baseload'].index.min(), dataset['weekly_baseload'].index.min())
     mymax = min(dataset['dataframe'].index.max(), dataset['daily_baseload'].index.max(), dataset['weekly_baseload'].index.max())
 
+    #These are half hourly
     dataset['dataframe'] = dataset['dataframe'].ix[mymin:mymax]
     dataset['daily_baseload'] = dataset['daily_baseload'].ix[mymin:mymax]
     dataset['weekly_baseload'] = dataset['weekly_baseload'].ix[mymin:mymax]
     dataset['daily_sum'] = dataset['daily_sum'].ix[mymin:mymax]
     dataset['weekly_sum'] = dataset['weekly_sum'].ix[mymin:mymax]
+
+    dataset['weekly_percent'] = (dataset['weekly_min']['consumption (kWh)'] * 336) / dataset['weekly_sum']['consumption (kWh)']
 
 for dataset in datasets:
 	dataset['daily'] = {}
@@ -101,16 +108,17 @@ raw_data_folder = os.path.join(output_folder, 'raw_data')
 if not os.path.exists(raw_data_folder):
     os.makedirs(raw_data_folder)
 
-print("plotting raw data")
-for i, dataset in enumerate(datasets):
-    df = dataset['dataframe']
-    fig, ax = plt.subplots(1, 1, figsize=(7, 4))
-    ax.plot(df.index, df['consumption (kWh)'], 'k-', lw=0.15)
-    fig.suptitle("%s\n" % dataset['lbl'])
-    ax.set_ylabel('consumption\n(kWh)')
-    plt.tight_layout()
-    plt.savefig(os.path.join(raw_data_folder, "%s.png" % dataset['lbl']))
-    plt.close(fig)
+if do_raw:
+    print("plotting raw data")
+    for i, dataset in enumerate(datasets):
+        df = dataset['dataframe']
+        fig, ax = plt.subplots(1, 1, figsize=(7, 4))
+        ax.plot(df.index, df['consumption (kWh)'], 'k-', lw=0.15)
+        fig.suptitle("%s\n" % dataset['lbl'])
+        ax.set_ylabel('consumption\n(kWh)')
+        plt.tight_layout()
+        plt.savefig(os.path.join(raw_data_folder, "%s.png" % dataset['lbl']))
+        plt.close(fig)
 
 
 fmt = dates.DateFormatter(fmt="%b\n%Y")
@@ -120,64 +128,68 @@ baseload_folder = os.path.join(output_folder, 'baseload')
 if not os.path.exists(baseload_folder):
     os.makedirs(baseload_folder)
 
-print("plotting baseload lines")
-for i, dataset in enumerate(datasets):
-    fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
-    ax.set_title("%s\n" % dataset['lbl'])
-    ax.plot(dataset['dataframe'].index, dataset['dataframe']['consumption (kWh)'], c='black', label="raw", lw=0.2, alpha=0.2)
-    ax.plot(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], c='blue', label="daily", lw=0.5)
-    ax.plot(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], c='red', label="weekly", lw=0.8)
-    ax.xaxis.set_major_locator(loc)
-    ax.xaxis.set_major_formatter(fmt)
-    ax.set_ylabel('minimum daily/weekly\nconsumption (kWh)')
-    leg = ax.legend(loc=2)
-    plt.tight_layout()
-    # plt.savefig("%s.png" % dataset['lbl'])
-    plt.savefig(os.path.join(baseload_folder, "%s line.png" % dataset['lbl']))
-    plt.close(fig)
+if do_baseload_lines:
+    print("plotting baseload lines")
+    for i, dataset in enumerate(datasets):
+        fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+        ax.set_title("%s\n" % dataset['lbl'])
+        ax.plot(dataset['dataframe'].index, dataset['dataframe']['consumption (kWh)'], c='black', label="raw", lw=0.2, alpha=0.2)
+        ax.plot(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], c='blue', label="daily", lw=0.5)
+        ax.plot(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], c='red', label="weekly", lw=0.8)
+        ax.xaxis.set_major_locator(loc)
+        ax.xaxis.set_major_formatter(fmt)
+        ax.set_ylabel('minimum daily/weekly\nconsumption (kWh)')
+        leg = ax.legend(loc=2)
+        plt.tight_layout()
+        # plt.savefig("%s.png" % dataset['lbl'])
+        plt.savefig(os.path.join(baseload_folder, "%s line.png" % dataset['lbl']))
+        plt.close(fig)
 	
 fmt = dates.DateFormatter(fmt="%b\n%Y")
 loc = dates.MonthLocator(interval=4)
 
-print("plotting baseload areas")
-for i, dataset in enumerate(datasets):
-    fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
-    ax.set_title("%s\n" % dataset['lbl'])
-    ax.plot(dataset['dataframe'].index, dataset['dataframe']['consumption (kWh)'], label='total load', color='blue', lw=0.25)
-    ax.plot(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], label='daily baseload', color='yellow', lw=0.25)
-    ax.plot(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], label='weekly baseload', color='red', lw=0.25)
-    ax.text(0.01, 0.1, "baseload as proportion of total consumption = %3.1f%%" % dataset['weekly']['baseload%'], transform=ax.transAxes)
-    ax.fill_between(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], dataset['dataframe']['consumption (kWh)'], label="occupied load", color='blue', alpha=0.5, lw=0.2)
-    ax.fill_between(dataset['daily_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], dataset['daily_baseload']['consumption (kWh)'], label="occupied load", color='yellow', alpha=0.5, lw=0.2)
-    ax.fill_between(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], label="baseload", color='red', alpha=0.5, lw=0.2)
-    ax.set_ylim(0, ax.get_ylim()[1])
-    ax.xaxis.set_major_locator(loc)
-    ax.xaxis.set_major_formatter(fmt)
-    ax.set_ylabel('weekly\nconsumption (kWh)')
-    leg = ax.legend(loc=2)
-    plt.tight_layout()
-    plt.savefig(os.path.join(baseload_folder, "%s area.png" % dataset['lbl']))
-    plt.close(fig)
+if do_baseload_areas:
+    print("plotting baseload areas")
+    for i, dataset in enumerate(datasets):
+        fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+        ax.set_title("%s\n" % dataset['lbl'])
+        ax.plot(dataset['dataframe'].index, dataset['dataframe']['consumption (kWh)'], label='total load', color='blue', lw=0.25)
+        ax.plot(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], label='daily baseload', color='yellow', lw=0.25)
+        ax.plot(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], label='weekly baseload', color='red', lw=0.25)
+        ax.text(0.01, 0.1, "baseload as proportion of total consumption = %3.1f%%" % dataset['weekly']['baseload%'], transform=ax.transAxes)
+        ax.fill_between(dataset['daily_baseload'].index, dataset['daily_baseload']['consumption (kWh)'], dataset['dataframe']['consumption (kWh)'], label="occupied load", color='blue', alpha=0.5, lw=0.2)
+        ax.fill_between(dataset['daily_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], dataset['daily_baseload']['consumption (kWh)'], label="occupied load", color='yellow', alpha=0.5, lw=0.2)
+        ax.fill_between(dataset['weekly_baseload'].index, dataset['weekly_baseload']['consumption (kWh)'], label="baseload", color='red', alpha=0.5, lw=0.2)
+        ax.set_ylim(0, ax.get_ylim()[1])
+        ax.xaxis.set_major_locator(loc)
+        ax.xaxis.set_major_formatter(fmt)
+        ax.set_ylabel('weekly\nconsumption (kWh)')
+        leg = ax.legend(loc=2)
+        plt.tight_layout()
+        plt.savefig(os.path.join(baseload_folder, "%s area.png" % dataset['lbl']))
+        plt.close(fig)
 
 
 hist_folder = os.path.join(output_folder, 'histogram')
 if not os.path.exists(hist_folder):
     os.makedirs(hist_folder)
 
-print("plotting histograms")
-for i, dataset in enumerate(datasets):    
-    fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
-    # df=DataFrame(dataset['weekly_min']['consumption (kWh)'], dataset['weekly_sum']['consumption (kWh)']) 
-    # df.hist(ax=axes, bins=25, stacked=True)  
-    n, bins, patches = ax.hist(dataset['dataframe']['consumption (kWh)'], bins=25, label='total load', color='blue', alpha=0.5)
-    ax.hist(dataset['weekly_baseload']['consumption (kWh)'], bins=bins, label='baseload', color='red', alpha=0.5)
-    ax.set_xlabel('consumption\n(kWh)')
-    ax.set_title (dataset['lbl'])
-    ax.set_xlim(0, ax.get_xlim()[1])
-    plt.tight_layout()
-    # plt.savefig("Histogram/%s.png" % dataset['lbl'])	
-    plt.savefig(os.path.join(hist_folder, "%s.png" % dataset['lbl']))
-    plt.close(fig)
+if do_hist:
+    print("plotting histograms")
+    nbins = 50
+    for i, dataset in enumerate(datasets):    
+        fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+        # df=DataFrame(dataset['weekly_min']['consumption (kWh)'], dataset['weekly_sum']['consumption (kWh)']) 
+        # df.hist(ax=axes, bins=25, stacked=True)  
+        n, bins, patches = ax.hist(dataset['dataframe']['consumption (kWh)'], bins=nbins, label='total load', color='blue', alpha=0.5)
+        ax.hist(dataset['weekly_baseload']['consumption (kWh)'], bins=bins, label='baseload', color='red', alpha=0.5)
+        ax.set_xlabel('consumption\n(kWh)')
+        ax.set_title (dataset['lbl'])
+        ax.set_xlim(0, ax.get_xlim()[1])
+        plt.tight_layout()
+        # plt.savefig("Histogram/%s.png" % dataset['lbl'])	
+        plt.savefig(os.path.join(hist_folder, "%s.png" % dataset['lbl']))
+        plt.close(fig)
 
 # import csv
 # with open('csv/output.csv','wb') as f:
@@ -205,28 +217,61 @@ for i, dataset in enumerate(datasets):
 		
 # fmt = dates.DateFormatter(fmt="%b\n%Y")
 # loc = plt.Locator(interval=datasets)
+
+
 		
 boxdata = []
+datasets.sort(key=lambda ds: ds['weekly']['total']['median'])
 for i, dataset in enumerate(datasets):
-	boxdata.append(dataset['weekly_min']['consumption (kWh)'])
-
-fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
-#data = ([dataset['lbl'], dataset['weekly']['baseload%']])
-ax.boxplot(boxdata)
-
+	boxdata.append(dataset['weekly_sum']['consumption (kWh)'])
 lbls = [''.join(c for c in d['lbl'] if is_number_or_cap(c)) for d in datasets]
 
-ax.set_labels=lbls#('Queens','H.A','K.b','C.C','J.W','16 N.W','A.C.C','A.L.C','B.L.P.S','Bel C.E',
-#'Bel N.C','B.L.C','C.P.S','C.S.S','DMH','E.L.C','F.P.S','I.I.S','I.J.S','K.F.PS','L.Leys','New L.C.','New Parks','New Walk','Spence Strt')
-# ax.set_labels=('Queens','H.A','K.b','C.C','J.W','16 N.W','A.C.C','A.L.C','B.L.P.S','Bel C.E',
-# 'Bel N.C','B.L.C','C.P.S','C.S.S','DMH','E.L.C','F.P.S')
-plt.xticks ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],ax.set_labels, fontsize=6,rotation=60)
-# plt.xticks ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],ax.set_labels, fontsize=6,rotation=60)
+fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+ax.boxplot(boxdata)
+ax.set_xticklabels(lbls)
+#plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],ax.set_labels, fontsize=6,rotation=60)
+ax.set_xlabel('Buildings')
+ax.set_ylabel('weekly usage (kWh)')	
+ax.set_title ('weekly total consumption')
+plt.tight_layout()
+plt.savefig(os.path.join(output_folder, "Box Chart total.png"))
+plt.close()	 
+
+boxdata = []
+datasets.sort(key=lambda ds: ds['weekly']['baseload']['median'])
+for i, dataset in enumerate(datasets):
+	boxdata.append(dataset['weekly_min']['consumption (kWh)'] * 336)
+lbls = [''.join(c for c in d['lbl'] if is_number_or_cap(c)) for d in datasets]
+
+fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+ax.boxplot(boxdata)
+ax.set_xticklabels(lbls)
+#plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],ax.set_labels, fontsize=6,rotation=60)
 ax.set_xlabel('Buildings')
 # ax.xaxis.set_major_locator(loc)
 # ax.xaxis.set_major_formatter(fmt)
 ax.set_ylabel('weekly usage (kWh)')	
 ax.set_title ('weekly baseload consumption')
 plt.tight_layout()
-plt.savefig(os.path.join(output_folder, "Box Chart.png"))
-# plt.close()	 
+plt.savefig(os.path.join(output_folder, "Box Chart baseload.png"))
+plt.close()
+
+boxdata = []
+datasets.sort(key=lambda ds: ds['weekly']['baseload%'])
+for i, dataset in enumerate(datasets):
+	boxdata.append(dataset['weekly_percent'])
+lbls = [''.join(c for c in d['lbl'] if is_number_or_cap(c)) for d in datasets]
+
+fig, ax = plt.subplots(1, 1, figsize=(7, 4), sharex=True)
+ax.boxplot(boxdata)
+ax.set_xticklabels(lbls)
+#plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],ax.set_labels, fontsize=6,rotation=60)
+ax.set_xlabel('Buildings')
+# ax.xaxis.set_major_locator(loc)
+# ax.xaxis.set_major_formatter(fmt)
+ax.set_ylabel('weekly baseload / usage (%)')
+ax.set_title ('weekly baseload percent')
+plt.tight_layout()
+plt.savefig(os.path.join(output_folder, "Box Chart percent.png"))
+plt.close()
+
